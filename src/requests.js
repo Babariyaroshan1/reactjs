0a440c6f9684558423a67b9e213e96edfb18fc8a
babariyaroshan1 <babariyaroshan123@gmail.com>
2025-05-20 17:28:51 +0530

    data

---
+++ src/requests.js
@@ -0,0 +1,10 @@
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+};
+
+export default requests;
